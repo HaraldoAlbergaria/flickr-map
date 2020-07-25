1b3c13978accf7ddd5c8ccaa620edb72d6a8b368
Haraldo Filho <haraldo.filho@gmail.com>
2020-07-25 10:47:27 -0300

    Changed link to 'mapbox_token' on groups

--- groups/setup-group.sh
+++ groups/setup-group.sh
@@ -3,7 +3,7 @@
 mkdir $1
 cd $1
 ln -s ../../api_credentials.py .
-ln -s ../../mapbox_token .
+ln -s ../mapbox_token .
 ln -s ../../header.html .
 ln -P ../generate-map.py
 echo "# group alias or id" > config.py
