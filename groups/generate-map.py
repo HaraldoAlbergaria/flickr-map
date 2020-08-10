2a95dab8a3dee015d7308fd448b593434183343c
Haraldo Filho <haraldo.filho@gmail.com>
2020-08-10 11:28:33 -0300

    Implemented incremental run (only process new photos) on groups script

--- groups/generate-map.py
+++ groups/generate-map.py
@@ -11,6 +11,7 @@ import flickrapi
 import json
 import os
 import sys
+import math
 
 # get full script's path
 run_path = os.path.dirname(os.path.realpath(__file__))
@@ -55,12 +56,12 @@ def isGeoTagged(photo):
 #===== MAIN CODE ==============================================================#
 
 # check if there is a header file and read it
-if os.path.exists("{}/header.html".format(run_path)):
-    header_file = open("{}/header.html".format(run_path))
-    header = header_file.readlines()
-    header_file.close()
+if os.path.exists("{}/map.html".format(run_path)):
+    map_file = open("{}/map.html".format(run_path))
+    map = map_file.readlines()
+    map_file.close()
 else:
-    print("ERROR: FATAL: File 'header.html' is missing. Unable to run.")
+    print("ERROR: FATAL: File 'map.html' is missing. Unable to run.")
     sys.exit()
 
 # check if there is a mapbox token file and get the token
@@ -87,8 +88,6 @@ except:
 
 coordinates = []
 
-print('###################### Flickr Map ######################')
-
 photos = flickr.groups.pools.getPhotos(api_key=api_key, group_id=group_id, per_page=photos_per_page)
 npages = int(photos['photos']['pages'])
 total = int(photos['photos']['total'])
@@ -104,85 +103,247 @@ if os.path.exists("{}/last_total.py".format(run_path)):
 os.system("echo \"number = {0}\" > {1}/last_total.py".format(total, run_path))
 
 # create output map file
-map_file = open("{}/map.html".format(run_path), 'w')
+index_file = open("{}/index.html".format(run_path), 'w')
 
 # read header and write to map
-for line in header:
+for line in map:
     if line == '    mapboxgl.accessToken = \'\';\n':
         # add mapbox access token
-        map_file.write("    mapboxgl.accessToken = \'{}\';\n".format(mapbox_token))
+        index_file.write("    mapboxgl.accessToken = \'{}\';\n".format(mapbox_token))
     else:
-        map_file.write(line)
+        index_file.write(line)
 
     if line == '<meta charset=\"utf-8\" />\n':
         # add map page title
-        map_file.write("<title>{} | Photos Map</title>\n".format(group_name))
+        index_file.write("<title>{} | Photos Map</title>\n".format(group_name))
+
+index_file.close()
 
 print('Extracting photo coordinates and ids...')
 
+# get number of pages to be processed
+npages = math.ceil(total/int(photos_per_page))
+
+# to be included on map
+n_photos = 0  # counts number of photos
+n_markers = 0 # counts number of markers
+
+# extracts only the photos below a number limit
 if npages > max_number_of_pages:
     npages = max_number_of_pages
     total = max_number_of_pages * int(photos_per_page);
     print("Extracting for the last {} photos".format(total))
 
-n = 0
-e = 0
-p = 0
-m = 0
-
+# process each page
 for pg in range(1, npages+1):
+
+    # get photos on the pool
     page = flickr.groups.pools.getPhotos(api_key=api_key, group_id=group_id, extras='geo,tags,url_sq', page=pg, per_page=photos_per_page)['photos']['photo']
+
     photos_in_page = len(page)
+
+    # process each photo on page
     for ph in range(0, photos_in_page):
-        n += 1
+
         photo = page[ph]
-        exists = False
+
+        # variable to store information if already exist a marker
+        # on the same photo's coordinates
+        marker_exists = False
+
+        # check if photo can be included on the map (according to privacy settings)
         if isGeoTagged(photo):
-            p += 1
+
+            n_photos += 1
+
+            # get coordinates from photo
+            longitude = float(photo['longitude'])
+            latitude = float(photo['latitude'])
+
+            # read each markers coordinates and append photo is case
+            # there is already a marker on the same coordinate
             for coord in coordinates:
-                if photo['longitude'] == coord[0][0] and photo['latitude'] == coord[0][1]:
+                if longitude == coord[0][0] and latitude == coord[0][1]:
                     coord[1].append([photo['owner'], photo['id'], photo['url_sq']])
-                    exists = True
+                    marker_exists = True
                     break
-            if not exists:
-                coordinates.append([[photo['longitude'], photo['latitude']], [[photo['owner'], photo['id'], photo['url_sq']]]])
-                m += 1
-        if p >= max_number_of_photos or m >= max_number_of_markers:
-            break
-    e += photos_in_page
-    print('Batch {0}/{1} | {2} photos in {3} markers'.format(pg, npages, p, m), end='\r')
-    if p >= max_number_of_photos:
+
+            # create a new marker to be added to the map
+            if not marker_exists:
+                coordinates.append([[longitude, latitude], [[photo['owner'], photo['id'], photo['url_sq']]]])
+                n_markers += 1
+
+        # stop processing photos if any limit was reached
+        if n_photos >= total or n_photos >= max_number_of_photos or n_markers >= max_number_of_markers:
+           break
+
+    print('Batch {0}/{1} | {2} photo(s) in {3} marker(s)'.format(pg, npages, n_photos, n_markers), end='\r')
+
+    # stop processing pages if any limit was reached
+    if n_photos >= total:
+        break
+    if n_photos >= max_number_of_photos:
         print("\nMaximum number of photos on map reached!", end='')
         break
-    if m >= max_number_of_markers:
+    if n_markers >= max_number_of_markers:
         print("\nMaximum number of markers on map reached!", end='')
         break
 
-if p == 0:
-    print('\nNo geo tagged photo on the group pool\nMap not generated')
+# stop and exit script if there is no photo to be added to the map
+if n_photos == 0:
+    if mode == 'photoset':
+        print('\nNo geo tagged photo on the user photoset\nMap not generated')
+    else:
+        print('\nNo geo tagged photo on the user photostream\nMap not generated')
     sys.exit()
 
-print('\nAdding markers to map...')
+print('\nAdding marker(s) to map...')
 
-m = 0
+# check if there is java script file with the markers on map already
+# and readt it otherwise created a new one
+if os.path.exists("{}/locations.js".format(run_path)):
+    locations_js_file = open("{}/locations.js".format(run_path))
+else:
+    locations_js_file = open("{}/locations.js".format(run_path), 'w')
+    locations_js_file.write("var locations = [\n")
+    locations_js_file.write("]\n")
+    locations_js_file.close()
+    locations_js_file = open("{}/locations.js".format(run_path))
+
+# read the file and store it
+locations_js_lines = locations_js_file.readlines()
+locations_js_file.close()
+
+# create a python file with the existing markers,
+# import it and delete it
+locations_py_file = open("{}/locations.py".format(run_path), 'w')
+locations_py_file.write("locations = [\n")
+for i in range(1, len(locations_js_lines)):
+    locations_py_file.write(locations_js_lines[i])
+locations_py_file.close()
+from locations import locations
+os.system("rm {}/locations.py".format(run_path))
+
+# create a new javascript file to store new markers
+locations_js_file = open("{}/locations.js".format(run_path), 'w')
+locations_js_file.write("var locations = [\n")
+
+# get the number of markers (locations) already on map
+n_locations = len(locations)
+if n_locations > 0:
+    print('Map already has {} marker(s)'.format(n_locations))
+
+# counts the number of new photos added to markers
+new_photos = 0
+
+# process each marker info already on map
+for loc in range(n_locations):
+
+    # get info for photos on marker
+    photos_info = locations[loc][1].replace('</div>','')
+    n_photos = int(locations[loc][2])
+
+    # get number of photos (coordinates) to be added to map
+    n_coords = len(coordinates)
+
+    # iterate over each coordinate
+    for coord in range(n_coords):
+
+        # if there is already a marker on the same coordinate
+        if coordinates[coord][0] == locations[loc][0]:
+
+            # read each photo already on the marker
+            for photo in coordinates[coord][1]:
+                photo_url = photos_base_url + photo[0]
+                thumb_url = photo[1]
+
+                # if the photo is not already on marker, add the photo to it
+                if photo_url not in photos_info:
+                    photos_info += "<a href=\"{0}\" target=\"_blank\"><img src=\"{1}\"/></a> ".format(photo_url, thumb_url)
+                    new_photos += 1
+
+            # remove photo info from
+            # coordinates to be added
+            coordinates.pop(coord)
+
+    photos_info += "</div>"
+
+    # update the number of photos on marker
+    n_photos += new_photos
+    locations[loc][1] = photos_info
+    locations[loc][2] = n_photos
+    locations_js_file.write("    {}".format(locations[loc]))
+
+    if len(coordinates) > 0:
+        locations_js_file.write(",\n")
+    else:
+        locations_js_file.write("\n")
+
+if new_photos > 0:
+    print('Added {} new photo(s) to existing markers'.format(new_photos))
+
+# reverse the coordinates order so
+# the newest ones go to the end
+coordinates.reverse()
+
+# check if there is remaining markers to be added
 n_markers = len(coordinates)
+if n_markers > 0:
+    print('{} new marker(s) will be added to the map'.format(n_markers))
+
+# remove the oldest locations to make
+# room for new markers without violate
+# the max number of markers limit
+new_locations_length = len(locations) + n_markers
+if new_locations_length >= max_number_of_markers:
+    new_locations_length = max_number_of_markers - n_markers
+    print('Max number of markers reached. Removing {} marker(s)...'.format(n_markers))
+    while len(locations) > new_locations_length:
+        locations.pop(0)
+
+new_markers = 0
+
+# iterate over each marker to be added
 for marker_info in coordinates:
-    m += 1
-    longitude = marker_info[0][0]
-    latitude = marker_info[0][1]
-    map_file.write("            [[{0}, {1}], \"<div style=\\\"max-height:410px;overflow:auto;\\\">".format(longitude, latitude))
-    p = 0
+
+    new_markers += 1
+
+    # get coordinates of the new marker
+    longitude = float(marker_info[0][0])
+    latitude = float(marker_info[0][1])
+
+    # write it to locations file
+    locations_js_file.write("    [[{0}, {1}], \'<div style=\"max-height:410px;overflow:auto;\">".format(longitude, latitude))
+
+    # counts number of photos on marker
+    n_photos = 0
+
+    # iterate over each photo
     for photo in marker_info[1]:
+
+        # add photo to marker, writing it to locations file
         photo_url = 'https://www.flickr.com/photos/{}/{}/in/pool-{}/'.format(photo[0], photo[1], config.group)
         thumb_url = photo[2]
-        map_file.write("<a href=\\\"{0}\\\" target=\\\"_blank\\\"><img src=\\\"{1}\\\"/></a> ".format(photo_url, thumb_url))
-        p += 1
-    print('Added {0}/{1}'.format(m, n_markers), end='\r')
-    map_file.write("</div>\", {}],\n".format(p))
+        locations_js_file.write("<a href=\"{0}\" target=\"_blank\"><img src=\"{1}\"/></a> ".format(photo_url, thumb_url))
+        n_photos += 1
 
-print('\nFinished!')
+    # finish marker writing to location file
+    locations_js_file.write("</div>\', {}]".format(n_photos))
+    if new_markers < n_markers:
+        locations_js_file.write(",\n")
+    else:
+        locations_js_file.write("\n")
 
-map_file.write("        ]\n\n        return locations;\n\n    }\n\n</script>\n\n")
-map_file.write("\n</body>\n</html>\n\n")
-map_file.close()
+    print('Added marker {0}/{1}'.format(new_markers, n_markers), end='\r')
+
+# finish script
+if new_markers > 0:
+    print('')
+else:
+    print('No new markers were added to the map')
+
+print('Finished!')
+
+locations_js_file.write("]\n")
+locations_js_file.close()
 
