46b0a49b4f3fcb247f33383c5c7279ecf817be07
Haraldo Filho <haraldo.filho@gmail.com>
2020-09-18 12:40:01 -0300

    Replaced mapbox token

--- example/mapbox_token.js
+++ example/mapbox_token.js
@@ -1,4 +1,4 @@
 // Include here the mapbox access token
 // Get one at: https://account.mapbox.com/access-tokens/create
 var mapbox_token =
-'pk.eyJ1IjoiaGFyYWxkby1maWxobyIsImEiOiJja2N4OXVncXYwOTVuMndudmJvMmZrdTlyIn0.HUHW2x_gaT7aWYVyLVtz0w'
+'pk.eyJ1IjoiaGFyYWxkby1maWxobyIsImEiOiJja2Y4ZTE4cjUwMDk0MnFwZGs0Y2phMTduIn0.__9dME5MxCpO524sjomw2w'
