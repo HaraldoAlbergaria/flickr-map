95a8568094ff33123d66917d77be4402b5706968
Haraldo Filho <haraldo.filho@gmail.com>
2020-09-21 12:50:58 -0300

    Updated example map

--- example/countries.py
+++ example/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'IT': ['Italy', 76, 125],
-  'BR': ['Brazil', 176, 395],
   'VA': ['Vatican City', 22, 31],
   'HR': ['Croatia', 184, 243],
-  'US': ['United States of America', 51, 132],
+  'BR': ['Brazil', 174, 392],
+  'IT': ['Italy', 76, 125],
+  'PY': ['Paraguay', 1, 1],
   'AR': ['Argentina', 20, 31],
-  'PY': ['Paraguay', 1, 1]
+  'US': ['United States of America', 51, 132]
 }
--- example/last_total.py
+++ example/last_total.py
@@ -1,1 +1,1 @@
-number = 1065
+number = 1069
