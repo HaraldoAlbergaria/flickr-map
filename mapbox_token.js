a713b98946bb9a846bebcf09673c19d933b3c7c8
HaraldoFilho <haraldo.filho@gmail.com>
2024-12-28 20:04:08 -0300

    Renamed not_found_place.py to not_found.py

--- mapbox_token.js
+++ mapbox_token.js
@@ -1,4 +1,4 @@
 // Include here the mapbox access token
 // Get one at: https://account.mapbox.com/access-tokens/create
 var mapbox_token =
-'pk.eyJ1IjoiaGFyYWxkby1maWxobyIsImEiOiJja2hsd3RubW0wMnRvMnBxeGNhaDR2cjBsIn0.gyAZne8_kr7cOcrvj_Ho6g'
+''
