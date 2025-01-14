337b3d5cf90a1fff9d7250cc250ac9935b147dab
HaraldoFilho <haraldo.filho@gmail.com>
2025-01-14 18:46:40 -0300

    Edited generate-map-data.py

--- docs/example/countries.py
+++ docs/example/countries.py
@@ -1,11 +1,11 @@
 countries_dict = {
-  'CL': ['Chile', 78, 198],
-  'UY': ['Uruguay', 36, 67],
-  'VA': ['Vatican City', 22, 31],
-  'US': ['United States', 50, 132],
-  'PY': ['Paraguay', 1, 1],
-  'IT': ['Italy', 77, 126],
+  'UY': ['Uruguay', 41, 72],
   'HR': ['Croatia', 184, 243],
+  'VA': ['Vatican City', 22, 31],
+  'CL': ['Chile', 78, 198],
   'BR': ['Brazil', 176, 394],
-  'AR': ['Argentina', 93, 176]
+  'PY': ['Paraguay', 1, 1],
+  'AR': ['Argentina', 99, 178],
+  'IT': ['Italy', 77, 126],
+  'US': ['United States', 50, 132]
 }
